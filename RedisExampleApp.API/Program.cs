07b33e89f9ef81634b0401400b89041185a7f7a1
mucahit.kamat <mucahit.kamat@dgpays.com>
2023-03-16 16:52:02 +0300

    up

--- RedisExampleApp.API/Program.cs
+++ RedisExampleApp.API/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using RedisExampleApp.API.Models;
 using RedisExampleApp.API.Repository;
 using RedisExampleApp.Cache;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,30 +12,37 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductRepository>(sp =>
+{
+	var appDbContext = sp.GetService<AppDbContext>();
+	var productRepository = new ProductRepository(appDbContext);
+	var redisService = sp.GetRequiredService<RedisService>();
+	return new ProductRepositoryWithCacheDecorator(productRepository, redisService);
+
+});
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    options.UseInMemoryDatabase("mydb");
+	options.UseInMemoryDatabase("mydb");
 });
 
 var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
-    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    dbContext.Database.EnsureCreated();
+	var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+	dbContext.Database.EnsureCreated();
 
 }
 builder.Services.AddSingleton<RedisService>(sp =>
 {
-    return new RedisService(builder.Configuration["CacheOptions:Url"]);
+	return new RedisService(builder.Configuration["CacheOptions:Url"]);
 });
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
